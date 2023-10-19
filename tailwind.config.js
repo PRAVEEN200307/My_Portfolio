c9ae92d4cf7a4caf4c8310e582c0ac78729e23a2
unknown <crouselearn@gmail.com>
2023-10-19 22:27:11 +0530

    side nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+         'light-blue':'#2c98f0',
+      },
+    },
   },
   plugins: [],
 }
