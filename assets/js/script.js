2fb0dc25badc14bfb5816474ffdda717e193a56c
unknown <crouselearn@gmail.com>
2023-10-21 18:14:54 +0530

    home page

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const sideNav=document.querySelector('.side-nav');
+    const btnNav=document.querySelector('#btnNav');
+    const OverlayScreen=document.querySelector('.navoverlay');
+
+    btnNav.addEventListener('click',()=>{
+        sideNav.classList.add('opens');
+    });
+
+    OverlayScreen.addEventListener('click',()=>{
+        sideNav.classList.remove('opens');
+    });
+});
